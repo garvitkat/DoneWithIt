ca2df1afd9c7720acb19986141473fea7c0e7401
garvitkat <garvitvanshkataria@gmail.com>
2021-01-31 04:41:24 +0530

    Navigation Theme

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import { NavigationContainer } from "@react-navigation/native";
 
 // import navigationTheme from "./app/navigation/navigationTheme";
 import AuthNavigator from "./app/navigation/AuthNavigator";
+import navigationTheme from "./app/navigation/navigationTheme";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <AuthNavigator />
+      <AuthNavigator theme={navigationTheme} />
     </NavigationContainer>
   );
 }
